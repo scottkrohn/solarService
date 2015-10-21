d4b91c4415f626cb0a3eb682dde8a0828eecfd18
Scott Krohn <skrohn86@gmail.com>
2015-10-21 11:50:58 -0700

    First commit

---
+++ MyXMLParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+
+namespace SolarService
+{
+	// This class is used to parse specific data out of an xml file passed to the constructor.
+	public class MyXMLParser
+	{
+		XmlDocument xmldoc;
+		public MyXMLParser(Stream xmlStream)
+		{
+			xmldoc = new XmlDocument();
+			xmldoc.Load(xmlStream);
+		}
+
+		// parse the annual average solar intensity for a location from xml file.
+		public double getAverageIntensity()
+		{
+			XmlNodeList nodes = xmldoc.DocumentElement.SelectNodes("//response/outputs/avg-dni");
+			double average = -1;
+			foreach(XmlNode node in nodes)
+			{
+				average = Convert.ToDouble(node.SelectSingleNode("annual").InnerText);
+			}
+			return average;
+		}
+
+		// parse the latitude of a zipcode from xml file.
+		public double getLatitude()
+		{
+			XmlNodeList nodes = xmldoc.DocumentElement.SelectNodes("//GeocodeResponse/result/geometry/location");
+			double latitude = -1;
+			foreach(XmlNode node in nodes)
+			{ 
+				latitude = Convert.ToDouble(node.SelectSingleNode("lat").InnerText);
+			}
+			return latitude;
+		}
+
+		// parse the longitude of a zipcode from xml file.
+		public double getLongitude()
+		{
+			XmlNodeList nodes = xmldoc.DocumentElement.SelectNodes("//GeocodeResponse/result/geometry/location");
+			double longitude = -1;
+			foreach(XmlNode node in nodes)
+			{ 
+				longitude = Convert.ToDouble(node.SelectSingleNode("lng").InnerText);
+			}
+			return longitude;
+		}
+
+		// parse the location information (city, county, state, country) from xml file.
+		public string getLocationData()
+		{
+			XmlNodeList nodes = xmldoc.DocumentElement.SelectNodes("//GeocodeResponse/result/address_component");
+			string locationString = "";
+			foreach(XmlNode node in nodes)
+			{
+				locationString += string.Format("{0}, " ,node.SelectSingleNode("long_name").InnerText);
+			}
+			return locationString.Substring(0, locationString.Length-2);
+		}
+
+	}
+}
---
+++ SolarService.svc.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SolarService
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+	// NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+	public class Service1 : IService1
+	{
+		public double getSolarIntensity(double lat, double lon)
+		{
+			string sURL = string.Format("https://developer.nrel.gov/api/solar/solar_resource/v1.xml?api_key=3e2vJlG6bRBWFQeFzwbYZFIYfDoU6Fk2XqhfoWeP&lat={0}&lon={1}", lat, lon);
+			WebRequest request_solar = WebRequest.Create(sURL);
+			Stream solarStream;
+			solarStream = request_solar.GetResponse().GetResponseStream();
+			MyXMLParser myParser = new MyXMLParser(solarStream);
+			return myParser.getAverageIntensity();
+		}
+
+		public Tuple<double, double> getLatLong(string zipcode)
+		{
+			// Convert the zip code into a latitude and logitide pair using the Google Geocode API.
+			string zURL = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}", zipcode);
+			WebRequest request_latlong = WebRequest.Create(zURL);
+			Stream latLongStream = request_latlong.GetResponse().GetResponseStream();
+			MyXMLParser zipcodeParser = new MyXMLParser(latLongStream);
+			double lat = zipcodeParser.getLatitude();
+			double lon = zipcodeParser.getLongitude();
+			return new Tuple<double,double>(lat, lon);
+		}
+
+		public string getLocationData(string zipcode)
+		{
+			string zURL = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}", zipcode);
+			WebRequest request_location = WebRequest.Create(zURL);
+			Stream locationStream = request_location.GetResponse().GetResponseStream();
+			MyXMLParser locationParser = new MyXMLParser(locationStream);
+			return locationParser.getLocationData();
+		}
+
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+	}
+}
